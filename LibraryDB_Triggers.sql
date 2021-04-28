d09fc87dc335cba54983d9e304d71cf643a7ec1e
Robert Palmer <r91palmer@gmail.com>
2021-04-28 14:19:03 -0500

    added a trigger that adds log when a book is checkout

--- LibraryDB_Triggers.sql
+++ LibraryDB_Triggers.sql
@@ -4,8 +4,15 @@ on tbl_BooksIssued
 for insert
 as
 begin
-	declare @MemberName varchar(30)
-
+	declare @FirstName varchar(30) = (select first_name from tbl_Members
+		join inserted on tbl_Members.id = inserted.member_id)
+	declare @LastName varchar(30) = (select last_name from tbl_Members
+		join inserted on tbl_Members.id = inserted.member_id)
+	declare @BookTitle varchar(50) = (select title from tbl_Books
+		join inserted on tbl_Books.id = inserted.book_id)
+	declare @CheckoutDate date = (select date_issued from inserted)
+	insert into tbl_BookLog(message)
+	values(concat(@FirstName,' ',@LastName,' checked out ',@BookTitle,' on ',@CheckoutDate))
 end
 go
 
