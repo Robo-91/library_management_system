e53c3a7290c71e3deca10f491b508f5b31d420e0
Robert Palmer <r91palmer@gmail.com>
2021-05-11 15:33:27 -0500

    Modified cte and insert statement to include booksissued id

--- SQL Files/libraryApp_Statements.sql
+++ SQL Files/libraryApp_Statements.sql
@@ -15,13 +15,11 @@ as
 )
 
 -- Use records from CTE to insert into overdue table with a rate of .30/day
-insert into tbl_Overdue(member_id,days_overdue,total_charge)
-select cte_MemberId, 
+insert into tbl_Overdue(issued_id,member_id,days_overdue,total_charge)
+select cte_BooksIssuedId,
+	cte_MemberId, 
 	cte_DaysIssued - cte_Membershiplength as [Days Overdue],
 	(cte_DaysIssued - cte_Membershiplength) * .30
 	from cte_DaysIssued;
 
--- Write Statement for Loading appropriate results set for FactFees Table
--- Add a column to tbl_Overdue that references the tbl_BooksIssued id.
--- The id is already in the cte
--- From there, you can write a statement for tbl_booksissued left join tbl_overdue
+
